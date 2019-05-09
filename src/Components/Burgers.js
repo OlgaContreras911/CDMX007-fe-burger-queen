f5e8aaa7191ee1f56a5eaa508b343804374605f8
OlgaContreras911 <rockersita.911@gmail.com>
2019-05-09 17:51:30 -0500

    se agregan los botones en la sección de hamburgesas

--- src/Components/Burgers.js
+++ src/Components/Burgers.js
@@ -1,12 +1,68 @@
 import React from "react";
-
-const Burgers =(props)=>{
+import '../Css/Burgers.css';
+import btnburger from "../Assets/btnburger.png"
+import btnburgerchicken from "../Assets/btnburgerchicken.png"
+import btnburgerveg from "../Assets/btnburgerveg.png"
+import btnegg from "../Assets/btn-egg.png"
+import btnburgersim from "../Assets/btnburgersim.png"
+import btnchesse from "../Assets/btnchesse.png"
+const Burgers = (props) => {
     return (
-    <div className="Container">
-    <h1>Hamburgesas</h1>
+<div className="Burger-total">
+    <div className="Burger-menu">
+    <button className="btn-burger">
+      <div className="circle">
+      <img className="burgerdoble" src={btnburgersim} /></div>
+      <div className="text">Sencilla Res</div>
+      <div className="price">$40</div>
+      </button>
+      <button className="btn-burger">
+      <div className="circle">
+      <img className="burgerdoble" src={btnburgersim} /></div>
+      <div className="text">Sencilla Pollo</div>
+      <div className="price">$40</div>
+      </button>
+      <button className="btn-burger">
+      <div className="circle">
+      <img className="burgerdoble" src={btnburgersim} /></div>
+      <div className="text">Sencilla Vegetariana</div>
+      <div className="price">$40</div>
+      </button>
+      <button className="btn-burger">
+      <div className="circle">
+      <img className="burgerdoble" src={btnchesse} /></div>
+      <div className="text">Extra Queso</div>
+      <div className="price">$40</div>
+      </button>
     </div>
-    )
+    <div className="Burger-menu2">
+      <button className="btn-burger">
+      <div className="circle">
+      <img className="burgerdoble" src={btnburger} /></div>
+      <div className="text">Doble Res</div>
+      <div className="price">$55</div>
+      </button>
+      <button className="btn-burger">
+      <div className="circle">
+      <img className="burgerdoble" src={btnburgerchicken} /></div>
+      <div className="text">Doble Pollo</div>
+      <div className="price">$55</div>
+      </button>
+      <button className="btn-burger">
+      <div className="circle">
+      <img className="burgerdoble" src={btnburgerveg} /></div>
+      <div className="text">Doble Vegetariana</div>
+      <div className="price">$55</div>
+      </button>
+      <button className="btn-burger">
+      <div className="circle">
+      <img className="burgerdoble" src={btnegg} /></div>
+      <div className="text">Huevo</div>
+      <div className="price">$15</div>
+      </button> 
+    </div> 
+</div>)
 }
 
 
- export default Burgers;
+export default Burgers;
